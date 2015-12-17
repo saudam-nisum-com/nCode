542b9056cf6bde4f0c826618b547aaea2fdc446b
Shiva Shankar Audam <shiva.shankar.audam@macys.com>
2015-12-17 19:20:01 +0530

    nCode SA : Changes for adding DB

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 
 var express = require('express'),
-    db = require('./db');
- 
-    
-   
-
- var app = express();
-     db.config.connect();
-
-
-var express = require('express');
-var app = express();
-var path = require('path');
-
+	app = express(),
+    db = require('./db'),
+	path = require('path');
+	db.config.connect();
 
 app.set('port', (process.env.PORT || 9999));
 app.use('/', express.static(path.join(__dirname,'public')));
@@ -20,26 +11,10 @@ app.get('/',function(req,res){
 	res.sendFile("index.html",{root:path.join(__dirname,"public/app/views")});
 });
 
-app.get('/addcode',function(request, response) {
-       
-     var query = db.config.query('SELECT * FROM codeshelf', function(req, res) {
-            console.log(res);
-            response.send(res);
-        });
-        console.log(query.sql);
-    });
-
-
-
+app.get('/addcode',function(request, response) {     
+});
 app.post('/getcode', function(request, response) {
-        
-        var query = db.config.query('insert into codeshelf(tname,pname,pst_date) values(' + "'" + tname + "'" + "," + "'" + pname + "'" + "," + "'" + pst_date + "'" + ');', function(req, res) {
-            console.log(res);
-            response.send(res);
-        });
-        console.log(query.sql);
-    });
-
+});
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
--- db.js
+++ db.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'root',
-    database: 'ncode',
+    password: '',
+    database: '',
     dateStrings:'date'
 });
 
