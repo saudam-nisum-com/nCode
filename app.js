567e55fbeaa4f77af66e940044bdea4e52992d30
Shiva Shankar Audam <shiva.shankar.audam@macys.com>
2015-12-18 12:40:30 +0530

    resolving conflicts

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 var express = require('express'),
-<<<<<<< Updated upstream
-	app = express(),
-=======
-
     app = express(),
->>>>>>> Stashed changes
     db = require('./db'),
     path = require('path');
-db.config.connect();
+	db.config.connect();
 
-<<<<<<< Updated upstream
-=======
-db = require('./db');
-
-var app = express();
-db.config.connect();
-
-
-var express = require('express');
-var app = express();
-var path = require('path');
-
-
-
->>>>>>> Stashed changes
 app.set('port', (process.env.PORT || 9999));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
@@ -40,38 +20,23 @@ app.get('/addcode', function(request, response) {
     console.log(query.sql);
 });
 
-
 app.post('/getcode', function(request, response) {
 
     var query = db.config.query("select * from codeshelf where temp_id =?", [temp_id], function(req, res) {
         //console.log(res);
         response.send(res);
     });
-
-
 });
-<<<<<<< Updated upstream
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-=======
 
-
-//new updates 
 app.put('/changes/:id', function(request, response) {
     console.log("inside put");
-
-
-
-
     var query = db.config.query('update codeshelf set template = ?,comments = ? where id = ?', [template, comments, id], function(req, res) {
         //console.log(res);
         response.send(res);
     });
-
     console.log(query.sql);
 });
 
-//deleting the existing sessions
 app.delete('/delete/:id', function(request, response) {
     console.log("inside delete");
     var ssid = request.params.id;
@@ -79,16 +44,7 @@ app.delete('/delete/:id', function(request, response) {
         console.log(res);
         response.send(res);
     })
-
->>>>>>> Stashed changes
 });
-
-
-
-
-
-
-
 
 app.listen(app.get('port'), function() {
     console.log('Server started: http://localhost:' + app.get('port') + '/');
